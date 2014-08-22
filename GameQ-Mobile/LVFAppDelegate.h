2a22bc8b882ecfeaf0bf41e6f36f03812a13dd9f
LuddeFro <ludvig.froberg@gmail.com>
2014-08-22 03:31:53 +0200

    added iRate

--- GameQ-Mobile/LVFAppDelegate.h
+++ GameQ-Mobile/LVFAppDelegate.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "LVFViewController.h"
 #import "LVFDefinitions.h"
+#import "iRate.h"
 
 
 @class  LVFViewController;
--- GameQ-Mobile/LVFAppDelegate.m
+++ GameQ-Mobile/LVFAppDelegate.m
@@ -76,6 +76,11 @@ didReceiveRemoteNotification:(NSDictionary *)userInfo
     [alert show];
 }
 
++ (void) initialize
+{
+    [iRate sharedInstance].previewMode = NO;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
